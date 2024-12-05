40c0fe5d6cfebd0ddb5a883c975b7d2951a22ead
Jrryy <jerry.om.95@gmail.com>
2024-12-05 16:53:14 +0100

    Days 1-5

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod days;
+
+use crate::days::*;
+use std::env::args;
+use std::time::Instant;
+
+const CHALLENGES: [fn(); 5] = [
+    day_01::challenge,
+    day_02::challenge,
+    day_03::challenge,
+    day_04::challenge,
+    day_05::challenge,
+];
+
+fn main() {
+    let day = args().nth(1);
+    match day {
+        Some(n) => {
+            let day = n.parse::<usize>().unwrap();
+            if day > CHALLENGES.len() {
+                println!("Day {} not yet implemented", n);
+            } else {
+                let now = Instant::now();
+                CHALLENGES[day - 1]();
+                println!("{} µs", now.elapsed().as_micros());
+            }
+        }
+        None => {
+            CHALLENGES.iter().for_each(|challenge| {
+                let now = Instant::now();
+                challenge();
+                println!("{} µs", now.elapsed().as_micros());
+            });
+        }
+    }
+}
