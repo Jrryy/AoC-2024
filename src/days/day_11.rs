d0666b348786a413e669933bd6758b644318a4bf
Jrryy <jerry.om.95@gmail.com>
2025-01-02 12:04:26 +0100

    Day 11 optimized and finished

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -5,34 +5,39 @@ pub fn challenge() {
     println!("-- Running Day 11 --");
     let input = read_to_string("inputs/11.txt").unwrap();
     let mut stones_map: HashMap<u64, u64> = HashMap::new();
-    let mut stones = input
+    input
         .strip_suffix("\n")
         .unwrap_or(input.as_str())
         .split_whitespace()
         .map(|s| s.parse::<u64>().unwrap())
-        .for_each(|s| stones_map.entry(s).or_insert(1) += 1);
+        .for_each(|s| *stones_map.entry(s).or_default() += 1);
 
     let mut new_stones: HashMap<u64, u64> = HashMap::new();
 
-    for _ in 0..25 {
-        stones_map..for_each(|&s| {
-            if s == 0 {
-                new_stones.push(1);
+    for i in 0..75 {
+        stones_map.iter().for_each(|(&k, &v)| {
+            if k == 0 {
+                *new_stones.entry(1).or_default() += v;
             } else {
-                let string_s = s.to_string();
-                if string_s.len() % 2 == 0 {
-                    let split_string_s = string_s.split_at(string_s.len() / 2);
-                    new_stones.push(split_string_s.0.parse::<u64>().unwrap());
-                    new_stones.push(split_string_s.1.parse::<u64>().unwrap());
+                let string_k = k.to_string();
+                if string_k.len() % 2 == 0 {
+                    let split_string_k = string_k.split_at(string_k.len() / 2);
+                    *new_stones
+                        .entry(split_string_k.0.parse::<u64>().unwrap())
+                        .or_default() += v;
+                    *new_stones
+                        .entry(split_string_k.1.parse::<u64>().unwrap())
+                        .or_default() += v;
                 } else {
-                    new_stones.push(s * 2024);
+                    *new_stones.entry(k * 2024).or_default() += v;
                 }
             }
         });
-        stones = new_stones.clone();
-        new_stones = Vec::new();
+        stones_map = new_stones.clone();
+        new_stones = HashMap::new();
+        if i == 24 {
+            println!("Part 1: {}", stones_map.values().sum::<u64>());
+        }
     }
-
-    println!("Part 1: {}", stones.len());
-    println!("Part 2: {}", 0);
+    println!("Part 2: {}", stones_map.values().sum::<u64>());
 }
